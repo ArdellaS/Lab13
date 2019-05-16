64948e0535c1da539cd483df26e0af778a82104a
ardella <ardella.shelton@gmail.com>
2019-05-16 07:40:55 -0400

    unfinihed code

---
+++ Roshambo/Player2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roshambo
+{
+    class Player2 : Player
+    {
+        private static string name;
+        private static string roshamboValue;
+
+        public Player2(string PlayerTwo)
+            : base(name)
+        {
+            Name = PlayerTwo;
+            
+        }
+        public override Roshambo GenerateRoshambo()
+        {
+            Random randomRoshamboValue = new Random();
+            return (Roshambo)randomRoshamboValue.Next(0,3);
+        }
+
+    }
+
+}
+
---
+++ Roshambo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Roshambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play rock, paper, scissors! Would you like an easy or a difficult opponent?");
+            var playerType = Console.ReadLine();
+
+            var playerOne = new Player1("Player One");
+            var playertwo = new Player2("Player Two");
+
+            var answer = "y";
+            do
+            {
+                if (playerType == "Easy")
+                {
+                    Console.Write("Rock, Paper, Scissor. Shoot! Enter your selection: ");
+                    string userInput = Console.ReadLine();
+                }
+                Console.WriteLine(playerOne.GenerateRoshambo());
+                Console.WriteLine(playertwo.GenerateRoshambo());
+
+
+                Console.WriteLine("Try again?");
+                answer = Console.ReadLine();
+            } while (answer == "y");
+            
+
+            
+            //if (playerType == "easy")
+
+            //{
+            //    do
+            //    {
+            //        Console.Write("Rock, Paper, Scissor. Shoot! Enter your selection: ");
+            //        string userInput = Console.ReadLine();
+                    
+                    
+            //    } while (true);
+            //    else
+            //    {
+            //        Console.WriteLine("invalid election");
+            //    }
+            //}
+
+        }
+    }
+}
